a402e7b978380d770a376b18aa0a78f7cfc4398f
Max Wittek <maxwittek@gmail.com>
2023-04-16 11:33:40 -0400

    Got calendar view working

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import React, { useCallback, useState } from "react";
 import ReactDOM from "react-dom";
+import ReactCalendar from "react-calendar";
+import "./week.css"
+
+const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const currentDayOfWeek = daysOfWeek[(new Date().getDay() - 1 + 7) % 7];
+
+interface DayProps {
+    day: string
+}
+
+const Day = (props: DayProps) => {
+    const classes = ["day-box"]
+    if(props.day == currentDayOfWeek) {
+        classes.push("today")
+    }
+    return (
+        <div className={classes.join(" ")} key={props.day} >
+            {props.day}
+        </div>
+    )
+}
 
 const App = (props: { message: string }) => {
     const [count, setCount] = useState(0);
     const increment = useCallback(() => {
         setCount(count => count + 1);
     }, [count]);
+    
+
     return(<>
+    <div className="week-view-container">
+        {daysOfWeek.map(day => (
+            <Day day={day} />
+        ))}
+    </div>
+        
         <h1>{props.message}</h1>
         <h2>Count: {count}</h2>
         <button onClick={increment}>Increment</button>
