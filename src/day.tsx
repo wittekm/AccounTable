6754c471710175e815a3c36903639ae0fb281c8c
Max Wittek <maxwittek@gmail.com>
2023-04-16 11:43:58 -0400

    Gotta add Tracks DB

--- src/day.tsx
+++ src/day.tsx
@@ -1,15 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import { currentDayOfWeek } from "./logic/calendar"
+
+interface DailyTracks {
+    [trackid: string]: boolean
+}
 
 interface DayProps {
     day: string
 }
 
 export const Day = (props: DayProps) => {
+    const [state, setState] = useState({} as DailyTracks);
+
     const classes = ["day-box"]
     if(props.day == currentDayOfWeek) {
         classes.push("today")
     }
+
     return (
         <div className={classes.join(" ")} key={props.day} >
             {props.day}
